20a2b22b7257c46a49f28ea2e071d735c8e8b8f9
Ethereal <dethereal@protonmail.com>
2023-09-18 18:07:12 +0330

    transfering the repo

---
+++ components/ui/logo.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Png from '@/public/images/logo.png'
+import Image from 'next/image';
+export default function Logo() {
+  return (
+    <Link href="/" className="block" aria-label="Cruip">
+
+       <img className="relative rounded-full" src="https://seerintelligence.com/images/logo.png" width={50} height={50} alt="Logo" />
+
+    </Link>
+  )
+}
